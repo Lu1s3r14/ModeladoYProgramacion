03e364d48731ace97dd3badae043271328022f69
lu1s3r14 <luis.enriqueesca@ciencias.unam.mx>
2026-09-15 19:31:52 -0600

    puerto personalizado

--- src/Prueba.cs
+++ src/Prueba.cs
@@ -4,7 +4,26 @@ using System;
 
 public class Prueba {
     public static void Main(string[] args) {
-	// Crea el servidor con un puerto por defecto 1234
+
+	// Puerto por defecto si no se pasa uno. 
+	int puerto = 1234;
+
+	if (args.Length>0) {
+	    try {
+		if (int.TryParse(args[0], out int puertoDado)) {
+		    puerto = puertoDado;
+		} else {
+		    Console.WriteLine($"{args[0]} no es un puerto válido.");
+		}
+	    } catch (Exception) {
+		Console.WriteLine($"Puerteo no Válido. Se usará el puerto por defecto (1234).");
+	    }
+	} else {
+	    Console.WriteLine($"Al no recibir puerto, se utilizará el predeterminado (1234).");
+	}
+
+
+	
 	ServidorProyecto1 mini = new ServidorProyecto1(1234);
 	mini.inicia();
     }
